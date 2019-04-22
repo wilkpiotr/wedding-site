b13bebd8a631dce6f8afd3713a6e32535e15f13a
Piotr Wilk <piotrwilk@interia.pl>
2019-04-22 21:12:43 +0200

    react-bootstrap added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import './App.scss';
+import Button from 'react-bootstrap/Button'
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+    return (<div className="App-header"><h1>Agata & Piotr</h1>
+    <Button variant="outline-info">Info</Button></div>
     );
   }
 }
