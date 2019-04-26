999592eb94d89b73d12ec5e9c023a631f21d2d66
Piotr Wilk <piotrwilk@interia.pl>
2019-04-26 23:24:46 +0200

    move of the images folder

--- src/Components/Gallery/data.js
+++ src/Components/Gallery/data.js
@@ -1,12 +1,26 @@
-export default [{path: '/images/1.jpg'},
-{path: '/images/2.jpg'},
-{path: '/images/3.jpg'},
-{path: '/images/4.jpg'},
-{path: '/images/5.jpg'},
-{path: '/images/6.jpg'},
-{path: '/images/7.jpg'},
-{path: '/images/8.jpg'},
-{path: '/images/9.jpg'},
-{path: '/images/10.jpg'},
-{path: '/images/11.jpg'},
-{path: '/images/12.jpg'}]
+import image1 from '../../images/1.jpg';
+import image2 from '../../images/2.jpg';
+import image3 from '../../images/3.jpg';
+import image4 from '../../images/4.jpg';
+import image5 from '../../images/5.jpg';
+import image6 from '../../images/6.jpg';
+import image7 from '../../images/7.jpg';
+import image8 from '../../images/8.jpg';
+import image9 from '../../images/9.jpg';
+import image10 from '../../images/10.jpg';
+import image11 from '../../images/11.jpg';
+import image12 from '../../images/12.jpg';
+
+export default [{path: image1},
+{path: image2},
+{path: image3},
+{path: image4},
+{path: image5},
+{path: image6},
+{path: image7},
+{path: image8},
+{path: image9},
+{path: image10},
+{path: image11},
+{path: image12},
+]
