a94397286d26aa26b167280972cd51e83ef3be29
Piotr Wilk <piotrwilk@interia.pl>
2019-05-12 21:44:58 +0200

    transition on hover at navbar

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -14,27 +14,27 @@ class NavDropdownExample extends Component {
       return (
         <Nav as="ul" className="justify-content-end fixed-top nav pr-5 text-uppercase" variant="pills" activeKey={this.state.activeKey} onSelect={k => this.handleSelect(k)}>
           <Nav.Item as="li">
-            <Nav.Link as="a" eventKey="1" href="#home">
+            <Nav.Link as="a" className="link " eventKey="1" href="#home">
               Home
             </Nav.Link>
           </Nav.Item>
           <Nav.Item as="li" className="test">
-            <Nav.Link as="a" eventKey="2" href="#time">
+            <Nav.Link as="a" className="link text-body" eventKey="2" href="#time">
               Czas i Miejsce
             </Nav.Link>
           </Nav.Item>
           <Nav.Item as="li">
-            <Nav.Link as="a" eventKey="3" href="#gallery">
+            <Nav.Link as="a" className="link text-body" eventKey="3" href="#gallery">
               Galeria
             </Nav.Link>
           </Nav.Item>
           <Nav.Item as="li">
-            <Nav.Link as="a" eventKey="4" href="#wishes">
+            <Nav.Link as="a" className="link text-body" eventKey="4" href="#wishes">
               Życzenia
             </Nav.Link>
           </Nav.Item>
           <Nav.Item as="li">
-            <Nav.Link as="a" eventKey="5" href="#footer">
+            <Nav.Link as="a" className="link text-body" eventKey="5" href="#footer">
               Kontakt
             </Nav.Link>
           </Nav.Item>
